428516b455e769742a15a208df6450371f0ad453
nikeshlama <nikeshlama76@gmail.com>
2020-12-06 14:08:45 +0545

    login form created

---
+++ app/src/main/java/com/nikesh/testproject/loginform.java
@@ -0,0 +1,15 @@
+package com.nikesh.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginform extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginform);
+        //hello this is loginform
+    }
+}
